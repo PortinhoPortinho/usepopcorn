3a7153b5e681fffd7c727ea5b32ceea8397f4b22
PortinhoPortinho <m.porto2508@gmail.com>
2026-05-02 12:26:31 -0300

    115 star rating creation

---
+++ src/StarRating.js
@@ -0,0 +1,27 @@
+const containerStyle = {
+  display: "flex",
+  gap: "16px",
+  alignItems: "center",
+};
+
+const starContainerStyle = {
+  display: "flex",
+  gap: "4px",
+};
+const textStyle = {
+  lineHeight: "1",
+  margin: "0",
+};
+
+export default function StarRating({ maxRating = 5 }) {
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }).map((_, i) => (
+          <span>S{i + 1}</span>
+        ))}
+      </div>
+      <p>10</p>
+    </div>
+  );
+}
