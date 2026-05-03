aee499aa5b16c596bba9fcc78f30fc4f39c5f09b
PortinhoPortinho <m.porto2508@gmail.com>
2026-05-03 14:08:05 -0300

    120 basic of proptypes

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 const containerStyle = {
   display: "flex",
   gap: "16px",
@@ -7,6 +8,16 @@ const containerStyle = {
 
 const starContainerStyle = {
   display: "flex",
+};
+
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  className: PropTypes.string,
+  messages: PropTypes.array,
+  defaultRating: PropTypes.number,
+  onSetRating: PropTypes.func,
 };
 
 export default function StarRating({
