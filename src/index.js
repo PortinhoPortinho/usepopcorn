fb258536fcf9edb0333e4f9ce96a5807022bcb14
PortinhoPortinho <m.porto2508@gmail.com>
2026-05-06 21:28:16 -0300

    141 and 142 how to not fetch data how useEffect solves it

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import StarRating from "./StarRating";
+// import App-v1 from "./App-v1";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["terrible", "bad", "okay", "great", "amazing"]}
-    />
+    /> */}
     <StarRating size={24} color="red" defaultRating={3} />
   </React.StrictMode>,
 );
