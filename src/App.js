add61fc70d3bd1dd20b0f2b778d1d44eef160e2b
PortinhoPortinho <m.porto2508@gmail.com>
2026-05-11 06:24:51 -0300

    168 refs to persist data between renders

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect, useState, use } from "react";
 import StarRating from "./StarRating";
 
 const average = (arr) =>
@@ -164,7 +164,7 @@ function Search({ query, setQuery }) {
   }
     document.addEventListener("keydown", callback);
     return () => document.addEventListener("keydown", callback);
-  }, []);
+  }, [setQuery] );
 
   return (
     <input
@@ -251,6 +251,13 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [movie, setMovie] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
+
+const countRef = useRef(0);
+
+useEffect( function() {
+ if (userRating) countRef.current++;
+},[userRating] );
+
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
   const watchedUserRating = watched.find(
     (movie) => movie.imdbID === selectedId,
@@ -284,6 +291,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(" ").at(0)),
       userRating: Number(userRating),
+      countRatingDecisions: countRef.current,
     };
     onAddWatched(newWatchedMovie);
     onCloseMovie();
